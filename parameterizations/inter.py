c5c20ffc7dd62cdc9ed17c6bd3ce7a5c05f22ee3
Eli <epugh@stanford.edu>
2020-06-08 16:35:43 -0400

    simplify, include xticks in params

--- parameterizations/inter.py
+++ parameterizations/inter.py
@@ -5,14 +5,11 @@ from scipy.interpolate import interp1d
 
 class Airfoil(object):
     def __init__(self, params):
-        params = np.array(params)
-        assert params.size % 2 == 0
-        xs = np.linspace(0,np.pi/2,1+params.size//2)
-        xs = np.array([(0.5*(1.0-np.cos(x))) for x in xs])
-        self.x = np.hstack([xs,[1-e for e in xs[::-1][1:]]])
-        self.y = np.zeros(params.size+1)
+        params = np.array(params)        
+        self.x = params[0]
+        self.y = np.zeros(params.shape[1])
         for i in range(1,self.y.size):
-            self.y[i] = self.y[i-1] + params[i-1]
+            self.y[i] = self.y[i-1] + params[1][i-1]
         self.f = interp1d(self.x, self.y, kind=1)
 
     def Z_up(self, X):
